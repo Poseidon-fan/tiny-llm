825477b732d29d03edd1f0859e46837189b3520d
Poseidon <18646154381@163.com>
2026-04-16 17:09:48 +0800

    feat: impl day2

--- src/tiny_llm/positional_encoding.py
+++ src/tiny_llm/positional_encoding.py
@@ -9,9 +9,36 @@ class RoPE:
         base: int = 10000,
         traditional: bool = False,
     ):
-        pass
+        i = mx.arange(0, dims // 2)
+        theta = 1.0 / (base ** (2 * i / dims))
+
+        positions = mx.arange(seq_len)
+        freqs = mx.outer(positions, theta)
+
+        self.cos_freqs = mx.cos(freqs)
+        self.sin_freqs = mx.sin(freqs)
+        self.traditional = traditional
 
     def __call__(
         self, x: mx.array, offset: list[slice] | slice | None = None
     ) -> mx.array:
-        pass
+        L = x.shape[1]
+        if offset is None:
+            cos = self.cos_freqs[:L, None, :]
+            sin = self.sin_freqs[:L, None, :]
+        else:
+            cos = self.cos_freqs[offset, None, :]
+            sin = self.sin_freqs[offset, None, :]
+        
+        if self.traditional:
+            x_pair = x.reshape(*x.shape[:-1], -1, 2)
+            out0 = x_pair[..., 0] * cos - x_pair[..., 1] * sin
+            out1 = x_pair[..., 0] * sin + x_pair[..., 1] * cos
+            return mx.stack([out0, out1], axis=-1).reshape(*x.shape)
+        else:
+            x1 = x[..., :x.shape[-1] // 2]
+            x2 = x[..., x.shape[-1] // 2:]
+            out0 = x1 * cos - x2 * sin
+            out1 = x1 * sin + x2 * cos
+            return mx.concatenate([out0, out1], axis=-1)
+
